17a4a5d46447851c22c000929bcd7d6aa4b2e7e0
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-25 15:27:40 -0300

    feat: add BlogPost model and migration with association to User

---
+++ src/database/models/blogpost.js
@@ -0,0 +1,20 @@
+const BlogPost = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: { type: DataTypes.INTEGER, foreignKey: true }
+  },
+  {
+    tableName: 'BlogPosts'
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+      { foreignKey: 'userId', as: 'user' });
+  };
+
+  return BlogPost;
+};
+
+module.exports = BlogPost;
