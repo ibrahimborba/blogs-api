48efcd6be8f77a41bcdf3f0d8ca5a81401c1451d
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-26 13:46:28 -0300

    refactor: simplify sequelize managed transaction

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,18 +1,14 @@
-const Sequelize = require('sequelize');
 const { Op } = require('sequelize');
-const config = require('../database/config/config');
-const { User, BlogPost, Category, PostCategory } = require('../database/models');
-
-const sequelize = new Sequelize(config.development);
+const { User, BlogPost, Category, PostCategory, sequelize } = require('../database/models');
 
 const add = async ({ userId, title, content, categoryIds }) => {
   const { count, rows } = await Category.findAndCountAll({ where: { id: categoryIds } });
   if (!count) return null;
 
-  const result = await sequelize.transaction(async (t) => {
-    const post = await BlogPost.create({ title, content, userId }, { transaction: t });
+  const result = await sequelize.transaction(async (transaction) => {
+    const post = await BlogPost.create({ title, content, userId }, { transaction });
     const postCategories = rows.map((category) => ({ postId: post.id, categoryId: category.id }));
-    await PostCategory.bulkCreate(postCategories, { transaction: t });
+    await PostCategory.bulkCreate(postCategories, { transaction });
 
     return post;
   });
