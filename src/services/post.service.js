a89b8033d4fbc9b4274fa9b5fe4c96280c2794c9
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-08-26 01:40:04 -0300

    refactor: remove unnecessary try/catch

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -5,23 +5,18 @@ const { BlogPost, Category, PostCategory } = require('../database/models');
 const sequelize = new Sequelize(config.development);
 
 const add = async ({ userId, title, content, categoryIds }) => {
-  try {
-    const { count, rows } = await Category.findAndCountAll({ where: { id: categoryIds } });
-    if (!count) return null;
+  const { count, rows } = await Category.findAndCountAll({ where: { id: categoryIds } });
+  if (!count) return null;
 
-    const result = await sequelize.transaction(async (t) => {
-      const post = await BlogPost.create({ title, content, userId }, { transaction: t });
-      const postCategories = rows.map((category) => ({ postId: post.id, categoryId: category.id }));
-      
-      await PostCategory.bulkCreate(postCategories, { transaction: t });
+  const result = await sequelize.transaction(async (t) => {
+    const post = await BlogPost.create({ title, content, userId }, { transaction: t });
+    const postCategories = rows.map((category) => ({ postId: post.id, categoryId: category.id }));
+    await PostCategory.bulkCreate(postCategories, { transaction: t });
 
-      return post;
-    });
-    return result;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+    return post;
+  });
+
+  return result;
 };
 
 module.exports = { add };
