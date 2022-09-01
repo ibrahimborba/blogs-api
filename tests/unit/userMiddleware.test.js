196854e74733c94772ee208bf7667d705b6579cd
Ibrahim Borba <ibrahim.borba@gmail.com>
2022-09-01 19:57:04 -0300

    fix: sonar duplication errors

--- tests/unit/userMiddleware.test.js
+++ tests/unit/userMiddleware.test.js
@@ -34,16 +34,18 @@ describe('User middleware', function () {
     });
 
     describe('Login with invalid password', function () {
-      const request = { body: { email: 'lewishamilton@gmail.com', password: '' } };
+      let response;
 
-      it(EXPECT_400,
-        async function () {
-        const response = await stubMiddleware(userMiddleware.loginValidator, request);
+      const request = { body: { email: 'lewishamilton@gmail.com', password: '' } };
+      before(async function () {
+        response = await stubMiddleware(userMiddleware.loginValidator, request);
+      });
+
+      it(EXPECT_400, async function () {
         expect(response.status.calledWith(400)).to.be.equal(true);
       });
       it('to be called with message with value "Some required fields are missing"',
         async function () {
-        const response = await stubMiddleware(userMiddleware.loginValidator, request);
         expect(response.json.calledWith({ message: 'Some required fields are missing' }))
           .to.be.equal(true);
       });
@@ -66,22 +68,22 @@ describe('User middleware', function () {
     });
 
     describe('Request with invalid displayName', function () {
+      let response;
+
       const request = { body: {
         displayName: 'Brett',
-        email: 'brett@email.com',
-        password: '123456',
-        image:
-        'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
       } };
+
+      before(async function () {
+        response = await stubMiddleware(userMiddleware.userValidator, request);
+      });
 
       it(EXPECT_400,
         async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
         expect(response.status.calledWith(400)).to.be.equal(true);
       });
       it('to be called with message with expected value',
         async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
         expect(response.json.calledWith({
           message: '"displayName" length must be at least 8 characters long', 
         }))
@@ -90,48 +92,45 @@ describe('User middleware', function () {
     });
 
     describe('Request with invalid email', function () {
+      let response;
+
       const request = { body: {
         displayName: 'Brett Wiltshire',
         email: 'brett',
-        password: '123456',
-        image:
-        'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
       } };
 
-      it(EXPECT_400,
-        async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
+      before(async function () {
+        response = await stubMiddleware(userMiddleware.userValidator, request);
+      });
+
+      it(EXPECT_400, async function () {
         expect(response.status.calledWith(400)).to.be.equal(true);
       });
-      it('to be called with message with expected value',
-        async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
+      it('to be called with message with expected value', async function () {
         expect(response.json.calledWith({ message: '"email" must be a valid email' }))
           .to.be.equal(true);
       });
     });
 
     describe('Request with invalid password', function () {
+      let response;
       const request = { body: {
         displayName: 'Brett Wiltshire',
         email: 'brett@email.com',
         password: '12345',
-        image:
-        'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
       } };
 
-      it(EXPECT_400,
-        async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
+      before(async function () {
+        response = await stubMiddleware(userMiddleware.userValidator, request);
+      });
+
+      it(EXPECT_400, async function () {
         expect(response.status.calledWith(400)).to.be.equal(true);
       });
-      it('to be called with message with expected value',
-        async function () {
-        const response = await stubMiddleware(userMiddleware.userValidator, request);
+      it('to be called with message with expected value', async function () {
         expect(response.json.calledWith({
           message: '"password" length must be at least 6 characters long', 
-        }))
-          .to.be.equals(true);
+        })).to.be.equals(true);
       });
     });
   });
